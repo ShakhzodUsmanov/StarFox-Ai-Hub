2ac3d1d538cb2bb95ffb0d5222c073ba0aa6bdda
ShakhzodUsmanov <shaxzodusmanov11@gmail.com>
2023-08-29 13:50:11 +0500

    Dashboard page

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -5,8 +5,19 @@ import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
 
 import { CgMenu } from "react-icons/cg";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div>
       <Sheet>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,16 +4,21 @@ import Link from "next/link";
 import Image from "next/image";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
 import {
   MdOutlineDashboard,
   MdChatBubbleOutline,
   MdOutlineVideocam,
   MdCode,
-  MdOutlineSettings
+  MdOutlineSettings,
 } from "react-icons/md";
 import { LuImage, LuMusic } from "react-icons/lu";
 
-const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+const montserrat = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
 
 const routes = [
   {
@@ -60,6 +65,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -77,7 +83,12 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10 "
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon
